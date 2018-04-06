813ae951a3dc9717ffa4ffdfe21593916438c222
Aaron Burk <aarondburk@gmail.com>
2018-04-06 17:18:03 -0500

    update cors again again again

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ server.use(morgan('combined'));
 // server.use(helmet());
 
 const corsOptions = {
-  "origin": "http://localhost:3000",
+  "origin": "http://www.mynotater.com",
   // "AllowedHeaders": ['Content-Type', 'Authorization', 'body'],
   "credentials": true,
   // "methods": ['GET', 'PUT', 'POST', 'DELETE']
